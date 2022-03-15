8db40b15000db8047b6a51279c7e49ad10f8e052
Justin McKeen <justin_mckeen@yahoo.com>
2022-03-14 18:36:45 -0600

    Added footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,13 @@
       </div>
     </div>      <!--\/ \/  is the 'props' from ProductList-->
     <ProductList :products="productss" />
-  </div>              <!--  /\ /\ /\ is a 'computed'-->
+           <!--  /\ /\ /\ is a 'computed'-->
+    <div class='bottom-container'>
+      <div class='source'> <a href='https://github.com/BYUCS260/grocery-store-bleeedbluefan'>Source</a></div>
+    </div>
+  </div>        
+       
+  
 </template>
 
 <script>
@@ -70,6 +76,15 @@ input {
   box-shadow: none !important;
   width: 100%;
   height: 40px;
+}
+
+.bottom-container {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    padding: 10px;
+    background-color: white;
+    border: 1px solid black;
 }
 </style>
 
